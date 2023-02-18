36e68816c44807a5d9dcd212fd987286d475c221
Mario Rodrigues Jr <102671736+MarioJr92@users.noreply.github.com>
2023-02-18 12:52:10 -0500

    created program beginning choices

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -3,4 +3,10 @@ using namespace std;
 
 int main()
 {
+  cout << "Once upon a time, you find yourself in a magical forest. You see a path that splits in two. Which way do you choose?" << endl;
+  cout << "1. Left" << endl;
+  cout << "2. Right" << endl;
+
+  int pathChoice;
+  cin >> pathChoice;
 }
